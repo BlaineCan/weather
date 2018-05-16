10e3e854f9e8ce819332b884fb34cfb106d0bb91
Blaine <blaine.cantrell@gmail.com>
2018-05-16 01:28:58 -0500

    used back ticks in url

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ if (navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function (position) {
         let lat = position.coords.latitude;
         let long = position.coords.longitude;
-        fetch('http://api.wunderground.com/api/aed684636d700ce7/geolookup/conditions/q/'+ lat +','+ long +'.json').then(function (response) {
+        fetch(`http://api.wunderground.com/api/aed684636d700ce7/geolookup/conditions/q/${lat},${long}.json`).then(function (response) {
             return response.json()
         }).then(function (data) {
             console.log(data);
