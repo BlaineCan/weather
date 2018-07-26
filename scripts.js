720bfd1871780d8a1eb7695a65557beaf517f926
Blaine Cantrell <blaine.cantrell@gmail.com>
2018-07-26 00:57:51 -0500

    Stuff was did

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function (position) {
-        let lat = position.coords.latitude;
-        let long = position.coords.longitude;
-        fetch(`http://api.wunderground.com/api/****apikey*****/geolookup/conditions/q/${lat},${long}.json`).then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            console.log(data);
-        })
-    })
-}
+let conditions = [
+  {loc: 'New York', temp: 75, high: 77, low: 62}
+];
+
+(function weather(conditions){
+  this.init = function(){
+    this.getWeather();
+  };
+  this.update = function(conditions){
+    temp.innerHTML = conditions[0].temp;
+    location.innerHTML = conditions[0].loc;
+    high.innerHTML = conditions[0].high;
+    low.innerHTML = conditions[0].low;
+  }
+
+  this.getWeather = function(){
+    temp = document.querySelector('#weather_current_temp');
+    loc = document.querySelector('#current_location');
+    high = document.querySelector('#weather_high_temp');
+    low = document.querySelector('#weather_low_temp');
+    update(conditions);
+  }
+})();
