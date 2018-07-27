fe751433993b6e1709cb3d79da11baa0c4e70940
blaineCantrell <blaine.cantrell@gmail.com>
2018-07-27 01:16:45 -0500

    puts and displays api data. Time for bed now.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,28 @@
 (function weather(){
   this.init = function(){
     this.data();
-    this.search();
   }
   this.data = function(){
-    let url = 'https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24';
+    let key = //Your api key goes here;
+    let url = 'http://api.wunderground.com/api/'+ key +'/conditions/q/TX/Dallas.json';
     fetch(url).then(
       function(response){
         if(response.status !== 200){
           console.log('Looks like there was a problem. Status code: ' + response.status);
           return;
         }
-        response.json().then(function(data){
-          console.log(data);
+        response.json().then(
+          function(data){
+            console.log(data);
+            let location = document.querySelector('#location');
+            let temperature = document.querySelector('#weather_current_temp');
+            let description = document.querySelector('#description');
+            location.innerHTML = data.current_observation.display_location.city;
+            temperature.innerHTML = Math.round(data.current_observation.temp_f);
+            description.innerHTML = data.current_observation.weather;
         });
       }
     )
-  }
-  this.search = function () {
-    let button = document.querySelector('#submit-button');
-    button.addEventListener('click', function () {
-      let form = document.querySelector('.form');
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        let value = document.querySelector('#input-search').value;
-        form.reset();
-        console.log(value);
-      })
-    })
   }
   this.init();
 })();
