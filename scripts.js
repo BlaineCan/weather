f4a89b093983bb9b50da20581ddfa32ce128accf
Whatchamazog <blaine.cantrell@gmail.com>
2018-08-28 14:07:44 -0500

    Beginning of stuff.

--- scripts.js
+++ scripts.js
@@ -1,32 +1,48 @@
+// (function weather(){
+//   this.init = function(){
+//     this.sendLocation();
+//   }
+//   this.sendLocation = function(){
+//     let button = document.querySelector('#submit-button');
+//     button.addEventListener('click', getLocation());
+//   }
+//   this.getLocation = function(){
+//       let form = document.querySelector('.form');
+//       form.addEventListener('submit', function(event){
+//         event.preventDefault();
+//         let value = document.querySelector('#input-search').value;
+//         form.reset();
+//         getData(value.split(' ').join('+'));
+//       })
+//   }
+//   this.getData = function(loc){
+//     let url = ' http://autocomplete.wunderground.com/aq?query=' + loc;
+//     fetch(url, {mode: 'cors'}).then(
+//       function (response) {
+//         console.log(response[0].name);
+//       }
+//     )
+//   }
+//   this.init();
+// })();
+
 (function weather(){
   this.init = function(){
-    this.sendLocation();
+    this.getlocation();
+    this.showPosition();
   }
-  this.sendLocation = function(){
-    let button = document.querySelector('#submit-button');
-    button.addEventListener('click', getLocation());
-  }
-  this.getLocation = function(){
-      let form = document.querySelector('.form');
-      form.addEventListener('submit', function(event){
-        event.preventDefault();
-        let value = document.querySelector('#input-search').value;
-        form.reset();
-        getData(value);
-        console.log(value);
-      })
-  }
-  this.getData = function(loc){
-    let http = new XMLHttpRequest();
-    let url = ' http://autocomplete.wunderground.com/aq?query=' + loc;
-    let method = 'GET';
-    console.log(url);
-    http.open(method, url);
-    http.onreadystatechange = function(){
-      if(http.readyState === XMLHttpRequest.DONE && http.status === 200){
-        console.log(JSON.parse(http.response));
-      }
+  this.getlocation = function(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation not supported");
     }
+  }
+  this.showPosition = function(position){
+    console.log("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
+  }
+  this.fetchdata = function(){
+    let url = "" 
   }
   this.init();
 })();
