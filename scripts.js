8e8155fcf0b70e647240d5de78ebb03e75ca0a73
blaineCantrell <blaine.cantrell@gmail.com>
2018-08-29 22:09:54 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
     this.data();
   }
   this.data = function(){
-    let key = //Your api key goes here;
+    let key = 'aed684636d700ce7';
     let url = 'http://api.wunderground.com/api/'+ key +'/conditions/q/TX/Dallas.json';
     fetch(url).then(
       function(response){
