6070a51e1a2e2e9763954b5861a65cfc3ee3a969
Whatchamazog <blaine.cantrell@gmail.com>
2018-10-05 15:23:48 -0500

    Easily changes to cleaning up the weather app.

--- scripts.js
+++ scripts.js
@@ -1,58 +1,17 @@
-//Just something to note: This is a hot mess. I do apologize. I'll clean it up later.
-//Thanks for stopping by and checking it out though.
-
-(function weather(){
-  this.init = function(){
-    this.getlocation();
+(function getCoords(){
+  if(!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
   }
-  this.getlocation = function(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(showPosition);
-      this.showPosition();
-    } else {
-      console.log("Geolocation not supported");
-    }
-  }
-  this.showPosition = function(position){
+  navigator.geolocation.getCurrentPosition((position)=>{
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    let key = 'aed684636d700ce7';
-    let geolookup = 'https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/'+ key +'/geolookup/q/'+ latitude +','+ longitude +'.json';
-    fetch(geolookup).then(
-      function (response) {
-        if(response.status !== 200){
-          console.log('Looks like there was a problem. Status code: ' + response.status);
-          return;
-        }
-        response.json().then(
-          function(data){
-            let state = data.location.state;
-            let city = data.location.city;
-            let conditions = 'https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/'+ key +'/conditions/q/'+ state +'/'+ city +'.json';
-            fetch(conditions).then(
-              function(response){
-                if(response.status !== 200){
-                  console.log('Looks like there was a problem. Status code: '+ response.status);
-                  return;
-                }
-                response.json().then(
-                  function(data){
-                    let location = document.querySelector('#location');
-                    let temperature = document.querySelector('#weather_current_temp');
-                    let description = document.querySelector('#description');
-                    let icon = document.querySelector('#icon');
-                    location.innerHTML = data.current_observation.display_location.city;
-                    temperature.innerHTML = Math.round(data.current_observation.temp_f);
-                    description.innerHTML = data.current_observation.weather;
-                    icon.setAttribute('src', data.current_observation.icon_url);
-                  }
-                )
-              }
-            )
-          }
-        )
-      }
-    )
-  }
-  this.init();
+
+    getLocation(latitude, longitude);
+    //console.log("latitude: " + latitude + " & " + "longitude: " + longitude);
+  })
 })();
+
+function getLocation(latitude, longitude){
+    console.log(latitude, longitude);
+}
