263832e4dd8280e3211faca6cd45200e6099e3ab
Whatchamazog <blaine.cantrell@gmail.com>
2018-10-06 15:50:32 -0500

    Pulling json

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+
+function showPosition(latitude, longitude){
+    //console.log(latitude, longitude);
+    let geolookup = 'https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/'+ key +'/geolookup/q/'+ latitude +','+ longitude +'.json';
+    fetch(geolookup).then((response)=>{
+      if(response.status !== 200){
+        console.log(response.status);
+        return;
+      }
+      console.log(geolookup);
+    })
+}
+
 (function getCoords(){
   if(!navigator.geolocation){
     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
@@ -7,11 +20,7 @@
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    getLocation(latitude, longitude);
+    showPosition(latitude, longitude);
     //console.log("latitude: " + latitude + " & " + "longitude: " + longitude);
   })
 })();
-
-function getLocation(latitude, longitude){
-    console.log(latitude, longitude);
-}
