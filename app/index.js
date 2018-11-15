c4f84a451fef8a6cb7af644d6ad6af9355684b94
Whatchamazog <blaine.cantrell@gmail.com>
2018-11-15 15:38:14 -0600

    Set up template

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
 const app = require('express')();
+const pug = require('pug');
 const scripts = require('./scripts');
-const routes = require('./routes');
-const key = require('./key');
+//const routes = require('./routes');
+//const key = require('./key');
 
-app.use('/', routes);
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+    res.render('index', scripts.home)
+})
+
+//app.use('/', routes);
 
 app.listen(8000)
--- app/scripts.js
+++ app/scripts.js
@@ -1,10 +1,18 @@
-const key = require('./key');
+//const key = require('./key');
+
+// module.exports = {
+//     searchQuery: ()=>{
+//         let state = 'texas';
+//         let city = 'dallas';
+//         let conditions = 'https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/'+ key +'/conditions/q/'+ state +'/'+ city +'.json';
+//         console.log(conditions);  
+//     }
+// }
 
 module.exports = {
-    searchQuery: ()=>{
-        let state = 'texas';
-        let city = 'dallas';
-        let conditions = 'https://cors-anywhere.herokuapp.com/http://api.wunderground.com/api/'+ key +'/conditions/q/'+ state +'/'+ city +'.json';
-        console.log(conditions);  
+    home: {
+        title: 'Title',
+        message: "Hello world!"
+
     }
 }
