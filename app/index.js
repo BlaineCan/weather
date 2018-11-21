03eeeeca2d08c0452a2d577e615156b81736f9fd
Whatchamazog <blaine.cantrell@gmail.com>
2018-11-21 17:24:22 -0600

    Getting the ball rolling

--- app/index.js
+++ app/index.js
@@ -1,16 +1,22 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
 const pug = require('pug');
 const scripts = require('./scripts');
-//const routes = require('./routes');
 const key = require('./key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(express.static('public'));
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
-    res.render('index', scripts.home)
+    res.render('index')
 })
 
-//app.use('/', routes);
+app.post('/', (req, res)=>{
+    res.render('index')
+    console.log(req.body.city)
+})
 
 app.listen(8000)
